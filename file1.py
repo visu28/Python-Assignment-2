c19a52682c76e5a72e47a253c14c0dae7a09e57f
visu28 <vishakhasolanki590@gmail.com>
2025-06-03 10:00:12 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,8 @@
+# Take an number input from the user
+number = int(input("Enter a number: "))
+
+# Check whether the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
---
+++ file2.py
@@ -0,0 +1,9 @@
+
+total_sum = 0
+
+
+for number in range(1, 51):
+    total_sum += number  
+
+# Display the final sum
+print("The sum of all number from 1 to 50 is:", total_sum)
